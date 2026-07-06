5cbc0b00bc3a5ba49f1c121df8e02393cbde7ec5
haruo-prog <haruo@nk-ts.jp>
2026-07-06 13:56:33 +0900

    Bump NK Score Manager to v1.2 beta orientation build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "jp.co.nkts.scoremanager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1-beta"
+        versionCode = 3
+        versionName = "1.2-beta"
     }
 
     compileOptions {
